8a84990351c30f546fd6c557d486c28aaccb53f8
Sandesh Jung Chhetri <sandeshjung21@gmail.com>
2023-07-29 13:42:40 +1000

    Lab completed

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+let cats=["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name)
+{
+    cats.push(name);
+}
+
+   
+function destructivelyPrependCat(name)
+{
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat()
+{
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat()
+{
+    cats.shift();
+}
+
+function appendCat(name)
+{
+    let rat=[...cats,name];
+    return rat;
+}
+
+function prependCat(name)
+{
+    let hat=[name,...cats];
+    return hat;
+}
+
+function removeLastCat()
+{
+    return cats.slice(0,cats.length-1);
+}
+
+function removeFirstCat()
+{
+    return cats.slice(1);
+}
